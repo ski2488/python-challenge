d8e5be598c814d0b0d80e973c4e87e3ad72e3b15
ski2488 <mike.raminski@ardonaghspecialty.onmicrosoft.com>
2022-11-06 08:03:41 -0600

    commit module work with final solution

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,71 @@
+#PyBank Challenge
+#Load packages
+import os
+import csv
 
+#Declare variables
+months = 0
+profit = 0
+cum_profit = 0
+months_list = []
+change = []
+
+#Import csv data
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        #print(row)
+        
+        #Count months
+        months = months + 1
+        
+        #Month-over-month profit change
+        if cum_profit == 0:
+            delta_profit = 0
+            profit = int(row[1])
+        else:
+            delta_profit = int(row[1]) - profit 
+            profit = int(row[1])
+        
+        #Cumulative profit
+        cum_profit = cum_profit + int(row[1])
+
+        #Build list of months
+        months_list.append(row[0])
+        #Build list of month-over-month profits
+        change.append(delta_profit)
+
+#Calculate non-iterated metrics
+avg_change = round(sum(change) / (len(change) - 1), 2)
+max_change = max(change)
+min_change = min(change)
+max_month = months_list[change.index(max_change)]
+min_month = months_list[change.index(min_change)]
+
+#Print results
+results1 = f"Total Months: {months}"
+results2 = f"Total: ${cum_profit}"
+results3 = f"Average Change: ${avg_change}" 
+results4 = f"Greatest Increase in Profits: {max_month} (${max_change})"
+results5 = f"Greatest Decrease in Profits: {min_month} (${min_change})"
+resultsfinal = [results1, results2, results3, results4, results5]
+
+for item in resultsfinal:
+    print(item)
+    print('\n')
+
+#Export to txt file
+with open('analysis/results.txt', 'w') as f:
+    for item in resultsfinal:
+        f.write(item)
+        f.write('\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,75 @@
+#PyPoll
+#Load packages
+import os
+import csv
 
+#Declare variables
+total_votes = 0
+candidate1 = "Charles Casper Stockham"
+candidate1_count = 0
+candidate2 = "Diana DeGette"
+candidate2_count = 0
+candidate3 = "Raymon Anthony Doane"
+candidate3_count = 0
+candidate_list = []
+
+#Import csv data
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        #print(row)
+
+        #Count total votes
+        total_votes = total_votes + 1
+
+        #Candidate votes
+        if row[2] == candidate1:
+            candidate1_count = candidate1_count + 1
+        elif row[2] == candidate2:
+            candidate2_count = candidate2_count + 1
+        else: candidate3_count = candidate3_count + 1
+
+        #Candidate winner
+        if candidate1_count > candidate2_count and candidate1_count > candidate3_count:
+            winner = candidate1
+        elif candidate2_count > candidate1_count and candidate2_count > candidate3_count:
+            winner = candidate2
+        else: winner = candidate3
+
+#Result variables
+candidate1_pct = round((candidate1_count / total_votes) * 100, 3)
+candidate2_pct = round((candidate2_count / total_votes) * 100, 3)
+candidate3_pct = round((candidate3_count / total_votes) * 100, 3)
+
+#Print results  
+results1 = f"Election Results"
+results2 = f"-------------------------"
+results3 = f"Total Votes: {total_votes}"
+results4 = f"-------------------------"
+results5 = f"{candidate1}: {candidate1_pct}% ({candidate1_count})"
+results6 = f"{candidate2}: {candidate2_pct}% ({candidate2_count})"
+results7 = f"{candidate3}: {candidate3_pct}% ({candidate3_count})"
+results8 = f"-------------------------"
+results9 = f"Winner: {winner}"
+results10 = f"-------------------------"
+resultsfinal = [results1, results2, results3, results4, results5, results6, results7, results8, results9, results10]
+
+for item in resultsfinal:
+    print(item)
+    print('\n')
+
+#Export to txt file
+with open('analysis/results.txt', 'w') as f:
+    for item in resultsfinal:
+        f.write(item)
+        f.write('\n')
